248c84f7f724a4fea1f232c0cf9d2e31008bdb46
siseniman <kukuhtriyuliarno@gmail.com>
2016-01-06 21:28:56 +0700

    versi 2

--- dss.sql
+++ dss.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Inang: 127.0.0.1
--- Waktu pembuatan: 26 Nov 2015 pada 15.05
+-- Waktu pembuatan: 21 Des 2015 pada 02.43
 -- Versi Server: 5.5.34
 -- Versi PHP: 5.4.22
 
@@ -39,18 +39,18 @@ CREATE TABLE IF NOT EXISTS `tbl_inv` (
   `rekomen` varchar(10) NOT NULL,
   `yn` enum('y','n') NOT NULL DEFAULT 'n',
   PRIMARY KEY (`id_inv`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
 
 --
 -- Dumping data untuk tabel `tbl_inv`
 --
 
 INSERT INTO `tbl_inv` (`id_inv`, `inves`, `bunga`, `df1`, `df2`, `irr`, `periode`, `kas`, `jml_periode`, `rekomen`, `yn`) VALUES
-(4, 10000, 9, 10, 11, 24.34, '1 >> 2 >> 3', '7000 >> 5000 >> 3000', 3, 'Terima', 'n'),
-(5, 20000, 10, 11, 12, 24.99, '1 >> 2 >> 3 >> 4', '12000 >> 10000 >> 6000 >> 5000', 4, 'Terima', 'y'),
-(6, 100000, 8, 8, 9, 20.97, '1 >> 2 >> 3 >> 4 >> 5', '70000 >> 40000 >> 20000 >> 10000 >> 9000', 5, 'Terima', 'y'),
+(5, 20000, 10, 11, 12, 24.99, '1 >> 2 >> 3 >> 4', '12000 >> 10000 >> 6000 >> 5000', 4, 'Terima', 'n'),
+(6, 100000, 8, 8, 9, 20.97, '1 >> 2 >> 3 >> 4 >> 5', '70000 >> 40000 >> 20000 >> 10000 >> 9000', 5, 'Terima', 'n'),
 (8, 300000, 10, 10, 11, 27.67, '1 >> 2 >> 3 >> 4', '200000 >> 150000 >> 100000 >> 90000', 4, 'Terima', 'n'),
-(9, 60000, 11, 12, 13, 22.36, '1 >> 2 >> 3 >> 4', '40000 >> 30000 >> 10000 >> 7000', 4, 'Terima', 'n');
+(9, 60000, 11, 12, 13, 22.36, '1 >> 2 >> 3 >> 4', '40000 >> 30000 >> 10000 >> 7000', 4, 'Terima', 'y'),
+(11, 10000, 10, 10, 11, 39.35, '1 >> 2 >> 3 >> 4', '9000 >> 8000 >> 6000 >> 4000', 4, 'Terima', 'y');
 
 -- --------------------------------------------------------
 
@@ -66,15 +66,15 @@ CREATE TABLE IF NOT EXISTS `tbl_run_inv` (
   `tgl_start` varchar(15) NOT NULL,
   `tgl_finish` varchar(15) NOT NULL,
   PRIMARY KEY (`id_run`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data untuk tabel `tbl_run_inv`
 --
 
 INSERT INTO `tbl_run_inv` (`id_run`, `id_inv`, `id_not`, `id_user`, `tgl_start`, `tgl_finish`) VALUES
-(9, 5, 5, 2, '2015-11-21', '2019-11-21'),
-(10, 0, 6, 6, '', '');
+(6, 0, 11, 6, '', ''),
+(7, 9, 9, 6, '2015-12-20', '2019-12-20');
 
 -- --------------------------------------------------------
 
@@ -104,7 +104,7 @@ CREATE TABLE IF NOT EXISTS `tbl_user` (
 INSERT INTO `tbl_user` (`id_user`, `username`, `password`, `fname`, `lname`, `address`, `email`, `phone`, `fb`, `twitter`, `level`) VALUES
 (1, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'admin', 'ok', 'admin', 'admin@gmail.com', '085642000000', 'admin', 'admin', 'admin'),
 (2, 'kukuh', 'a2f995b2f42394ea0358748fc0d738bad0780883', 'Kukuh', 'Triyuliarno', 'Tegal', 'kukuh@gmail.com', '085642590466', 'kukuh', 'kukuh', 'eksekutif'),
-(6, 'pelanggan', '597a445e77ecd913c415f2010823b7dc8095ec5c', 'pelanggan', 'ok', 'pelanggan', 'pelanggan@gmail.com', '089677777777', 'pelanggan', 'pelanggan', 'eksekutif');
+(6, 'pelanggan', '597a445e77ecd913c415f2010823b7dc8095ec5c', 'pelanggan', 'ok', 'Semarang', 'pelanggan@gmail.com', '089677777777', 'pelanggan', 'pelanggan', 'eksekutif');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
